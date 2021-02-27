a5674a53d981b30b99c561cea755489e3fb10636
Juan_Isturiz <juan.isturiz@correo.unimet.edu.ve>
2021-02-27 19:45:03 -0400

    Puse el JFileChooser

--- src/proyecto_1/DataManage.java
+++ src/proyecto_1/DataManage.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 /**
@@ -16,114 +17,132 @@ import javax.swing.JOptionPane;
  * @author yunch
  */
 public class DataManage {
-    public DataManage(){
-        
+
+    public DataManage() {
+
     }
-    public Graph readCSV(){
+
+    public Graph readCSV() {
         String doc;
         doc = "Test Packages\\movies.csv";
         String act = "";
         String mov = "";
         String star = "";
         List movies = new List();
-        List actors= new List();
+        List actors = new List();
         String line;
-        File docu = new File(doc);
-        try{
-            if (!docu.exists()){
-                docu.createNewFile();
-            } else{
-                FileReader fr = new FileReader(docu);
-                BufferedReader br = new BufferedReader(fr);
-                while ((line = br.readLine())!= null){
-                    if (!line.isEmpty()){
-                        mov += line + "\n";
-                        
-                    }
-                }
-                if(!"".equals(mov)){
-                    String[] movi = mov.split("\n");
-                    for (int i = 0; i<movi.length;i++){
-                        if (!movi[i].equals("id,title,year")){
-                            String [] data = movi[i].split(",");
-                            movies.append( Integer.parseInt(data[0]),data[1],Integer.parseInt(data[2]));
+        /**
+         * File docu = new File(doc);*
+         */
+        File docu;
+        JFileChooser select = new JFileChooser();
+        select.setCurrentDirectory(new File("Test Packages"));
+        select.setDialogTitle("Seleccione el archivo con la información de las películas...");
+        if (select.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            docu = select.getSelectedFile();
+            try {
+                if (!docu.exists()) {
+                    docu.createNewFile();
+                } else {
+                    FileReader fr = new FileReader(docu);
+                    BufferedReader br = new BufferedReader(fr);
+                    while ((line = br.readLine()) != null) {
+                        if (!line.isEmpty()) {
+                            mov += line + "\n";
+
                         }
-                        
                     }
-                fr.close();
-                br.close();
-                
+                    if (!"".equals(mov)) {
+                        String[] movi = mov.split("\n");
+                        for (String movi1 : movi) {
+                            if (!movi1.equals("id,title,year")) {
+                                String[] data = movi1.split(",");
+                                movies.append(Integer.parseInt(data[0]), data[1], Integer.parseInt(data[2]));
+                            }
+                        }
+                        fr.close();
+                        br.close();
+
+                    }
                 }
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(null, "Error while reading");
             }
-            }catch(IOException e){
-            JOptionPane.showMessageDialog(null, "Error while reading");
+        }
+        select = new JFileChooser();
+        select.setCurrentDirectory(new File("Test Packages"));
+        select.setDialogTitle("Seleccione el archivo con la información de los actores...");
+        if (select.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            docu = select.getSelectedFile();
+            try {
+                if (!docu.exists()) {
+                    docu.createNewFile();
+                } else {
+                    BufferedReader br;
+                    try (FileReader fr = new FileReader(docu)) {
+                        br = new BufferedReader(fr);
+                        while ((line = br.readLine()) != null) {
+                            if (!line.isEmpty()) {
+                                act += line + "\n";
+
+                            }
+                        }
+                        if (!"".equals(act)) {
+                            String[] acto = act.split("\n");
+                            for (String acto1 : acto) {
+                                if (!acto1.equals("id,name,birth")) {
+                                    String[] data = acto1.split(",");
+                                    actors.append(Integer.parseInt(data[0]), data[1], Integer.parseInt(data[2]));
+                                }
+                            }
+                        }
+                    }
+                    br.close();
+
+                }
+            } catch (IOException | NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Error while reading");
             }
-            doc ="Test Packages\\people.csv";
-            docu = new File(doc);
-        try{
-            if (!docu.exists()){
-                docu.createNewFile();
-            } else{
-                FileReader fr = new FileReader(docu);
-                BufferedReader br = new BufferedReader(fr);
-                while ((line = br.readLine())!= null){
-                    if (!line.isEmpty()){
-                        act += line + "\n";
-                        
-                    }
-                }
-                if(!"".equals(act)){
-                    String[] acto = act.split("\n");
-                    for (int i = 0; i<acto.length;i++){
-                        if (!acto[i].equals("id,name,birth")){
-                            String [] data = acto[i].split(",");
-                            actors.append(Integer.parseInt(data[0]),data[1],Integer.parseInt(data[2]));
-                        }
-                        
-                    }
-                }
-                fr.close();
-                br.close();
-            
         }
-        }catch(IOException | NumberFormatException e){
+        select = new JFileChooser();
+        select.setCurrentDirectory(new File("Test Packages"));
+        select.setDialogTitle("Seleccione el archivo con la información la participación de actores en películas...");
+        if (select.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            docu = select.getSelectedFile();
+        try {
+            if (!docu.exists()) {
+                docu.createNewFile();
+            } else {
+                BufferedReader br;
+                try (FileReader fr = new FileReader(docu)) {
+                    br = new BufferedReader(fr);
+                    while ((line = br.readLine()) != null) {
+                        if (!line.isEmpty()) {
+                            star += line + "\n";
+
+                        }
+                    }
+                    if (!"".equals(star)) {
+                        String[] str = star.split("\n");
+                        for (String str1 : str) {
+                            if (!str1.equals("person_id,movie_id")) {
+                                String[] data = str1.split(",");
+                                Node starring = actors.searchId(Integer.parseInt(data[0]));
+                                String[] peli = (movies.searchId(Integer.parseInt(data[1]))).getData().split(", ");
+                                starring.addEdge(Integer.parseInt(peli[0]), peli[1], Integer.parseInt(peli[2]));
+                            }
+                        }
+                    }
+                }
+                br.close();
+
+            }
+        } catch (IOException | NumberFormatException e) {
             JOptionPane.showMessageDialog(null, "Error while reading");
         }
-        doc ="Test Packages\\stars.csv";
-            docu = new File(doc);
-        try{
-            if (!docu.exists()){
-                docu.createNewFile();
-            } else{
-                FileReader fr = new FileReader(docu);
-                BufferedReader br = new BufferedReader(fr);
-                while ((line = br.readLine())!= null){
-                    if (!line.isEmpty()){
-                        star += line + "\n";
-                        
-                    }
-                }
-                if(!"".equals(star)){
-                    String[] str = star.split("\n");
-                    for (int i = 0; i<str.length;i++){
-                        if (!str[i].equals("person_id,movie_id") && !str[i].equals(null)){
-                            String [] data = str[i].split(",");
-                            Node starring = actors.searchId(Integer.parseInt(data[0]));
-                            String []peli = (movies.searchId(Integer.parseInt(data[1]))).getData().split(", ");
-                            starring.addEdge(Integer.parseInt(peli[0]), peli[1],Integer.parseInt(peli[2]));
-                        }
-                        
-                    }
-                }
-                fr.close();
-                br.close();
-            
-        }
-        }catch(IOException | NumberFormatException e){
-            JOptionPane.showMessageDialog(null, "Error while reading");
         }
         Graph grafo = new Graph(actors);
         return grafo;
-        }
-    
+    }
+
 }
